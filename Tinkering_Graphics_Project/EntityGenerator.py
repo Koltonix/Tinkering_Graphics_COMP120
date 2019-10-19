013e85e19788aa56deca10f481898c7e933d916f
Christopher Robertson <christopherprobertson@gmail.com>
2019-10-19 14:01:56 +0100

    Starting using docstrings (PEP257) to document my function and classes, but is still incomplete. Finalised the classes to create a new shape with ease as well as combining it with the earlier implemented image saving to save the image.

--- Tinkering_Graphics_Project/EntityGenerator.py
+++ Tinkering_Graphics_Project/EntityGenerator.py
@@ -9,9 +9,15 @@ FPS = 30
 PROGRAM_CAPTION = "Entity Generator"
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
+SCREEN_CENTRE = ((WINDOW_WIDTH * .5), (WINDOW_HEIGHT * .5))
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+shapes_to_render = []
 
 
 def main():
@@ -20,21 +26,30 @@ def main():
     main_screen = (pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)))
     pygame.display.set_caption(PROGRAM_CAPTION)
 
+    set_shapes()
+
     is_running = True
 
     while is_running:
         main_screen.fill(BLACK)
         check_for_quit()
 
-        #rectangle = Rectangle((WINDOW_WIDTH * .5, WINDOW_HEIGHT * .5), WHITE, pygame.Rect(0, 0, 5, 5))
-        #rectangle.draw_shape(main_screen)
+        draw_shapes(main_screen, shapes_to_render)
 
         if get_input():
-            image = pygame.image.load(str(os.getcwd()) + "\\Images\\" + "Cactus.png")
-            save_image(image, "New_Cactus.png", os.getcwd())
+            # image = pygame.image.load(str(os.getcwd()) + "\\Images\\" + "Cactus.png")
+            save_image(main_screen, "NewImage.png", os.getcwd())
 
         FPS_CLOCK.tick(FPS)
         pygame.display.update()
+
+
+def set_shapes():
+    centre_body = Circle(SCREEN_CENTRE, BLUE, 50)
+    shapes_to_render.append(centre_body)
+
+    square_head = Rectangle((SCREEN_CENTRE[0], SCREEN_CENTRE[1] - 50), WHITE, pygame.Rect(0, 0, 50, 50))
+    shapes_to_render.append(square_head)
 
 
 def save_image(image, file_name="", path=os.path):
@@ -45,7 +60,7 @@ def save_image(image, file_name="", path=os.path):
 
 
 def get_random_colour():
-    # Returns a random colour from the entire colour spectrum
+    """ Returns a random colour from the entire colour spectrum """
     colour = [0, 0, 0]
     for i in range(0, 2):
         colour[i] = random.randint(0, 255)
@@ -53,14 +68,13 @@ def get_random_colour():
     return tuple(colour)
 
 
-def draw_shapes(main_display, shapes=[]):
+def draw_shapes(main_display, shapes):
     for i in range(0, len(shapes)):
-        pygame.draw.polygon(main_display, shapes[i].colour, shapes[i].rect)
-        # Can make it blit and recentre here
+        shapes[i].draw_shape(main_display)
 
 
 def get_input():
-    # Returns a boolean to state if any button has been pressed
+    """ Returns a boolean to state if any button has been pressed """
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_p:
@@ -83,16 +97,33 @@ def terminate_program():
 
 
 class Shape:
+    """
+    The highest parent class that has the variables and function
+    signatures for the child classes to use and adapt
+    """
     def __init__(self, position=(0, 0), colour=(0, 0, 0)):
+        """ A constructor for Shape that assigns required parameters """
         self.position = position
         self.colour = colour
 
     def draw_shape(self, main_screen=pygame.Surface):
+        """
+        A separate function that is being declared, so that it may be used by
+        the child classes, but is not being used here since Shape is generic
+        """
         pass
 
 
 class Rectangle(Shape):
+    """
+    A child class to Shape which stores more variables to
+    allow the instantiation of a rectangle like shape
+    """
     def __init__(self, position=(0, 0), colour=(0, 0, 0), bounds=pygame.Rect(0, 0, 0, 0)):
+        """
+        The constructor of the Rectangle which takes in a pygame.Rect() parameter to be
+        used when creating a rectangle. It also runs the parent Shape class constructor too
+        """
         super(Rectangle, self).__init__(position, colour)
 
         bounds.center = (position[0], position[1])
@@ -105,13 +136,26 @@ class Rectangle(Shape):
 
 class Circle(Shape):
     def __init__(self, position=(0, 0), colour=(0, 0, 0), radius=0):
-        super(Circle, self).__init__()
+        super(Circle, self).__init__(position, colour)
         self.radius = radius
 
     def draw_shape(self, main_screen=pygame.Surface):
-        # COME BACK AND FIX IT NOT DRAWING
         super(Circle, self).draw_shape(main_screen)
-        pygame.draw.circle(main_screen, self.colour, self.position, self.radius)
+        pygame.draw.circle(main_screen, self.colour, (int(self.position[0]), int(self.position[1])), self.radius)
+
+
+class Polygon(Shape):
+    # Position is removed since bounds deals with it
+    def __init__(self, colour=(0, 0, 0), bounds=[]):
+        super(Polygon, self).__init__(colour)
+        self.bounds = bounds
+
+        # TO GO ABOUT THE POLYGON USE A SEPARATE DISPLAY TO FILL IN
+        # USING THE COORDS IN RANGE
+
+    def draw_shape(self, main_screen=pygame.Surface):
+        super(Polygon, self).draw_shape(main_screen)
+        pygame.draw.polygon(main_screen, self.colour, self.bounds)
 
 
 if __name__ == '__main__':
