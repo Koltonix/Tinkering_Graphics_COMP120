d70ee6152f29cab068c78c868b623c66ce0e493a
Christopher Robertson <christopherprobertson@gmail.com>
2019-10-24 00:48:13 +0100

    Altered James Heathage's Contract while doing a bit of pair programming with him. I adjusted his system so that it would consistently update what the colour alterations were by using a global to store the data rather than using returns and parameters which did not allow for this. I also moved my of EntityGenerator.py into /Entity Generator/EntityGenerator.py.

--- Tinkering_Graphics_Project/TileGenerator.py
+++ Tinkering_Graphics_Project/TileGenerator.py
@@ -1,3 +1,8 @@
+"""
+
+Pair Programmed with Christopher Robertson
+"""
+
 import sys
 import pygame
 
@@ -13,8 +18,11 @@ TILE_HEIGHT = 200
 TILE_WIDTH = 200
 tile_colour = 255, 255, 255
 
+current_colour_overlay = (0, 0, 0)
+
 
 def main():
+    global current_colour_overlay
     pygame.init()
 
     main_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -23,12 +31,18 @@ def main():
     is_running = True
 
     while is_running:
+        print(current_colour_overlay)
         check_for_quit()
         main_window.fill(WINDOW_COLOUR)
-        main_window.blit(new_surface, (0, 0))
 
         draw_tile(new_surface)
+
         check_for_input(new_surface)
+        print(current_colour_overlay)
+        if current_colour_overlay is not None:
+            set_image_pixels(new_surface, current_colour_overlay)
+
+        main_window.blit(new_surface, (0, 0))
 
         FPS_CLOCK.tick(FPS)
         pygame.display.update()
@@ -40,20 +54,28 @@ def draw_tile(new_surface):
 
 
 # Checks for User Input for which Tile to Generate. Struggling to get it to output.
-def check_for_input(surface=pygame.Surface((1, 1))):
-    pixel = pygame.Color(0, 0, 0)
-
+def check_for_input(image=pygame.Surface):
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
+            global current_colour_overlay
+
             if event.key == pygame.K_q:
-                print('hello')
-                for x in range(surface.get_width()):
-                    for y in range(surface.get_height()):
-                        pixel = surface.get_at((x, y))
-                        surface.set_at(
-                            (x, y),
-                            pygame.color(int(pixel.r * 0.5), pixel.g, pixel.b)
-                        )
+                current_colour_overlay = (255, 255, 255)
+
+            elif event.key == pygame.K_w:
+                current_colour_overlay = (55, 55, 55)
+
+
+def set_image_pixels(image=pygame.Surface, colour=(0, 0, 0)):
+    for x in range(image.get_width()):
+        for y in range(image.get_height()):
+            pixel = image.get_at((x, y))
+            adjusted_pixel_colour = (pixel[0] - colour[0], pixel[1] - colour[1], pixel[2] - colour[2])
+
+            if adjusted_pixel_colour[0] * adjusted_pixel_colour[1] * adjusted_pixel_colour[2] < 0:
+                continue
+
+            image.set_at((x, y), adjusted_pixel_colour)
 
 
 def check_for_quit():
