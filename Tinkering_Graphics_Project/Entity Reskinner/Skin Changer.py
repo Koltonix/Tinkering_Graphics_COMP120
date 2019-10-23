6e60cbfccf722c960de31c2815f04ca3e8ee90cd
Alex Williamson <alex_williamson@rocketmail.com>
2019-10-23 13:28:59 +0100

    Finished Entity Reskinner/Skin Changer.py

--- Tinkering_Graphics_Project/Entity Reskinner/Skin Changer.py
+++ Tinkering_Graphics_Project/Entity Reskinner/Skin Changer.py
@@ -1,40 +1,70 @@
 import pygame
+import sys
+import os
 
 
-# Returns the position of a pixel in a surface
-def get_pixel(x,y,surface):
-    return surface.get_at((x,y))
+# finds the pixel at x,y in the image
+def get_pixel(x,y,image):
+    return image.get_at((x,y))
 
 
-# Changes the colour of each pixel to be more red/green/blue as indicated by the user
-def colour_change(surface):
-    pixel_matrix = pygame.PixelArray(surface)
-    for x in range(surface.get_width()):
-        for y in range(surface.get_height()):
-            value = get_Pixel(x, y, surface)
-            if desired_colour == "Blue":
-                pixel_matrix[x, y] = (value.b, value.g * 0.5, value.r * 0.5)
-            elif desired_colour == "Green":
-                pixel_matrix[x, y] = (value.b * 0.5, value.g, value.r*0.5)
-            elif desired_colour == "Red":
-                pixel_matrix[x, y] = (value.b * 0.5, value.g * 0.5, value.r)
-    return surface
+# finds the pixels in the image that match the unwanted colour
+def colour_get(image):
+    pixel_matrix = pygame.PixelArray(image)
+    # for each pixel in the image
+    for x in range(image.get_width()):
+        for y in range(image.get_height()):
+            pixel = get_pixel(x, y, image)
+            # check if the pixel is the unwanted colour
+            if unwanted_colour == "blue":
+                if pixel.b - 50 >= pixel.g + pixel.r:
+                    # if it is, change it to the desired colour
+                    pixel_matrix[x, y] = colour_change()
+            elif unwanted_colour == "green":
+                if pixel.g -50 > pixel.b and pixel.g -50 > pixel.r:
+                    pixel_matrix[x, y] = colour_change()
+            elif unwanted_colour == "red":
+                if pixel.r - 50> pixel.b + pixel.g:
+                    pixel_matrix[x, y] = colour_change()
+            elif unwanted_colour == "yellow":
+                if (pixel.r + pixel.g)/2 >= pixel.b + 100:
+                    pixel_matrix[x, y] = colour_change()
+            else:
+                sys.exit("That is not a valid colour to change. ")
+    return image
 
 
-# Loads an image from a filepath and call colour_change
-def image_load(filename,surface):
-    newimage = pygame.image.load(filename)
-    newimage = colour_change(newimage)
-    surface.blit(newimage,(0,0))
+# changes the specified pixel into the desired colour
+def colour_change():
+    if desired_colour == "blue":
+        new_pixel = (0, 0, 200)
+    elif desired_colour == "green":
+        new_pixel = (0, 200, 0)
+    elif desired_colour == "red":
+        new_pixel = (200, 0, 0)
+    elif desired_colour == "yellow":
+        new_pixel = (200, 200, 0)
+    else:
+        sys.exit("That is not a valid colour to change to.")
+    return new_pixel
 
 
-# the main body of code
+# loads the image and calls the colour get function
+def image_load(filename):
+    try:
+        new_image = pygame.image.load(filename)
+    except:
+        sys.exit("That is an incorrect filepath")
+    new_image = colour_get(new_image)
+    return new_image
+
+
+# asks the user for the image, the colour to change, and the colour to change it to.
+# It then saves the colour changed image in the current directory as "colourChanged.png".
 pygame.init()
 image_filepath = input("enter image filepath: ")
-desired_colour = input("Red, Blue or Green? ")
-main_window = pygame.display.set_mode((750,750))
-running = True
-surface1 = pygame.Surface((750, 750))
-main_window.blit(surface1,(0,0))
-image_load(image_filepath,surface1)
-
+unwanted_colour = input("Do you want to change Red, Blue, Green, or Yellow? ")
+unwanted_colour = unwanted_colour.lower()
+desired_colour = input("Do you want to change it to Red, Blue, Green, or Yellow? ")
+desired_colour = desired_colour.lower()
+pygame.image.save(image_load(image_filepath), os.getcwd() + "\\colourChanged.png")
